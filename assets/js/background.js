5c9c91e22663e31a381b13f2c260046169e687c8
uryu_myao <myao.jpn@gmail.com>
2022-08-10 16:22:40 +0900

    Githubに移行

---
+++ assets/js/background.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// WorldTimeAPI
+// http://worldtimeapi.org/
+
+// const mainData = fetch(
+//   'http://worldtimeapi.org/api/timezone/Asia/Tokyo'
+// ).then((res) => res.json());
+
+// console.log(mainData);
+// console.log(mainData.abbreviation);
+
+// Dark Mode 切り替え
+const toggleLightDarkMode = (function () {
+  let db_body = document.querySelector('#db-body'),
+    triggler = document.querySelector('.btn-mode');
+  let darkMode = false;
+  let toggleMode = function () {
+    if (!darkMode) {
+      db_body.classList.add('--dm');
+      darkMode = true;
+    } else {
+      db_body.classList.remove('--dm');
+      darkMode = false;
+    }
+  };
+  let bindActions = function () {
+    triggler.addEventListener('click', toggleMode, false);
+  };
+  let init = function () {
+    bindActions();
+  };
+  return { init: init };
+})();
+toggleLightDarkMode.init();
